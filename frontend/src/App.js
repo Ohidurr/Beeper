85a3b635b7c58c41cb0ecde30677a9b4d4b29b1a
Ohidurr <OhidurRahman96@gmail.com>
2020-06-02 21:21:37 -0400

    fix backend, express running, add react router dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,13 @@ import React, { createContext } from 'react';
 import logo from './logo.svg';
 import './App.css';
 export const Context = createContext()
+import { Route, Switch } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
+    <Switch>
       <h1>Beeper</h1>
         <p>Be heard</p>
 
