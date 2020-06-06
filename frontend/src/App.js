3f515d67c6928a54481421ceb9e8a957631990aa
Ohidurr <OhidurRahman96@gmail.com>
2020-06-06 13:04:01 -0400

    something

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,20 @@
 import React, { createContext } from 'react';
 import logo from './logo.svg';
 import './App.css';
-export const Context = createContext()
 import { Route, Switch } from 'react-router-dom';
+export const Context = createContext()
 
 
 function App() {
   return (
     <div className="App">
-    <Switch>
       <h1>Beeper</h1>
         <p>Be heard</p>
-
+    <Switch>
+    <Route path={"/feed"}>
+    <feedPage/>
+    </Route>
+</Switch>
     </div>
   );
 }
