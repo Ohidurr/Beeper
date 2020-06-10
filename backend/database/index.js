c73c8acb587928eda2e2a2127a367fabe488c578
Ohidurr <OhidurRahman96@gmail.com>
2020-06-09 21:41:51 -0400

    add dotenv in backend index

--- backend/database/index.js
+++ backend/database/index.js
@@ -1,3 +1,5 @@
+require("dotenv").config()
 const pgp = require('pg-promise')({});
-const db = pgp("postgress://localhost:5432/beeper_db");
+const db = pgp(process.env.DATABASE_URL)
+// const db = pgp("postgress://localhost:5432/beeper_db");
 module.exports = db;
