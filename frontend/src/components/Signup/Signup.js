139e257de869ba383b7cee3f3cc636a20439dc23
Ohidurr <ohidurrahman96@gmail.com>
2020-09-29 19:26:35 -0400

    organizing components

---
+++ frontend/src/components/Signup/Signup.js
@@ -0,0 +1,14 @@
+import React from "react";
+import SignUpForm from "./form/SignUpForm";
+
+const SignUp = () => {
+  return (
+    <div className="signUp">
+      <div className="topDiv">
+        <SignUpForm />
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
---
+++ frontend/src/components/Signup/form/SignUpForm.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import { useHistory } from 'react-router-dom';
+
+const SignUp = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [name, setName] = useState("");
+    
+    return(
+        <form>
+
+        </form>
+    )
+}
+
+export default SignUp
